d5a46ca677b54ab2f776bd0e3b6eac5177be4936
MadeleineJonesUVA <mej8nnm@virginia.edu>
2021-09-08 15:59:16 -0400

    Madeleine edits Madeleine

--- Week3_graph3.R
+++ Week3_graph3.R
@@ -12,7 +12,9 @@ library(ggplot2)
 
 beach_plot <- ggplot(beaches, aes(rainfall, temperature)) +
   geom_line()
-beach_plot
 
 
 # STEP 3: Madeleine
+finished_plot <- beach_plot + ggtitle("Temperatue vs Rainfall for Beaches") + 
+  xlab("Temperature") + ylab("Rainfall") + theme_bw()
+finished_plot
